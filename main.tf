5482c511c18900328dc1e5d4f592fc9ef2c257f5
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-14 13:06:13 +0300

    adding cluster autoscaler

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cluster_autoscaler" {
-  source = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/Cluster-Autoscaler"
+  source = "github.com/mohamedezz96/Terraform-Modules/EKS-Tools/Cluster-AutoScaler"
   cluster_name = var.cluster_name
   eks_issuer = data.aws_eks_cluster.eks_data.identity[0].oidc[0].issuer
   cluster_autoscaler_version = "9.36.0"
